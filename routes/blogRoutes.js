2d3280ccd641b16cfa61ea9e3759fadd652ffec2
Arsal Naeem <arsalnaeem1@gmail.com>
2025-09-01 16:13:22 +0500

    feat: add cloudinary setup for blogs

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blogController");
+const {
+    uploadToCloudinary,
+    multerUpload,
+} = require("../middleware/cloudinary");
 
 router.get("/blogs", blogController.getAllBlogs);
-router.get("/:id", blogController.getBlogById)
-router.post("/create", blogController.createBlog);
-router.put("/edit/:id", blogController.editBlog);
+router.get("/:id", blogController.getBlogById);
+router.post("/create", multerUpload.single("image"), uploadToCloudinary, blogController.createBlog);
+router.put("/edit/:id", multerUpload.single("image"), uploadToCloudinary, blogController.editBlog);
 router.delete("/delete/:id", blogController.deleteBlog);
 
 module.exports = router;
