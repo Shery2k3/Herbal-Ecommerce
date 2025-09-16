d5a80634aaf60219386122dd46a5148d2f85f08e
Shahryar Khan <shahryar2k3@gmail.com>
2025-09-17 02:28:17 +0500

    fix: update API_BASE_URL to point to the production server

--- Client/src/constants.js
+++ Client/src/constants.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "https://server.houseofdumplings.com.pk/nodeserver"
 // const API_BASE_URL = "https://restaurant-server-copy-production.up.railway.app"
-const API_BASE_URL = "http://localhost:3000"
-// const API_BASE_URL = "https://demo-restaurant-server-production.up.railway.app"
+// const API_BASE_URL = "http://localhost:3000"
+const API_BASE_URL = "https://herbal-ecommerce-server-production.up.railway.app"
 export default API_BASE_URL
